55395dca451385a44f5194adc47adea9039a17cd
Sandra Larsson <sandral@mg.lan>
2019-02-07 19:50:32 +0100

    todoapp

--- cambro/Webbsystem och ramverk/uppgift1/js/todoApp.js
+++ cambro/Webbsystem och ramverk/uppgift1/js/todoApp.js
@@ -1,153 +1,133 @@
 /**
  * App for making a simple todo list
- * Author: 
+ * Author: Sandra Larsson
  *
  * @requires jQuery
  * @version 0.0.1
  */
-$(document).ready(function () {
+$(document).ready(function() {
+  var TodoApp = (function() {
+    // Properties
+    var Todo = function(id, name, completeted) {
+      this.id = id;
+      this.name = name;
+      this.completeted = completeted;
+    };
 
-    var TodoApp = (function () {
-        // Properties
-        var Todo = function (id, name, completeted) {
-            this.id = id;
-            this.name = name;
-            this.completeted = completeted;
-        };
+    var todos = [];
 
-        var todos = [];
+    var increment = 0;
 
-        var increment = 0;
+    var addTodo = function(todo) {
+      var newTodo = $("<li>" + todo.name + "<span>X</span></li>");
 
-        var addTodo = function (todo) {
-            console.log(todo.name);
-
-            var newTodo = $('<li>' + todo.name + '</li>');
-            console.log(newTodo);
-            newTodo.data('id', todo.id);
-            newTodo.data('name', todo.name);
-            newTodo.data('completed', todo.completed);
-            /*  if (todo.completed) {
+      newTodo.data("id", todo.id);
+      newTodo.data("name", todo.name);
+      newTodo.data("completed", todo.completed);
+      /*  if (todo.completed) {
                  newTodo.addClass("done");
              } */
-            $('.list').append(newTodo);
-            todos.push(todo);
+      $(".list").append(newTodo);
+      todos.push(todo);
+    };
 
-        }
+    var addTodos = function(todos) {
+      //add todos to page here
+      todos.forEach(function(todo) {
+        addTodo(todo);
+      });
+    };
 
-        var addTodos = function (todos) {
-            //add todos to page here
-            todos.forEach(function (todo) {
-                addTodo(todo);
-            });
-        }
+    var createTodo = function() {
+      if (!$("#todoInput").is(":empty")) {
+        console.log("hit");
+        var userInput = $("#todoInput").val();
+        var newItem = new Todo(increment, userInput, false);
+        addTodo(newItem);
+        increment++;
+      } else {
+        console.log("miss");
+        $("#todoInput")
+          .attr("placeholder", "Du måste fylla i")
+          .placeholder();
+      }
+    };
 
+    var updateTodo = function(todo) {
+      var isDone = !todo.data("completed");
+      todo.toggleClass("done");
+      todo.data("completed", isDone);
+    };
 
+    var saveTodos = function(todos) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
 
-        var createTodo = function () {
-            //send request to create new todo
-            var userInput = $('#todoInput').val();
-            var newItem = new Todo(increment, userInput, false);
-            addTodo(newItem);
-            increment++;
+    var initTodos = function() {
+      var storedTodos = JSON.parse(localStorage.getItem("todos"));
 
-            /* 
-                        $.post('/api/todos', {
-                                name: usrInput
-                            })
-                            .then(function (newTodo) {
-                                $('#todoInput').val('');
-                                addTodo(newTodo);
-                            })
-                            .catch(function (err) {
-                                console.log(err);
-                            }) */
-        }
+      if (storedTodos) {
+        storedTodos.forEach(function(todo) {
+          addTodo(todo);
+        });
+      }
+    };
 
+    var clearTodos = function(todos) {
+      //Radera array
+      while (todos.length) {
+        todos.pop();
+        console.log("deleted");
+      }
+      //Radera localstorage
+      localStorage.clear();
+    };
 
-        /*   var editItem = function (id, description) {
-              for (var i = 0; i < data.length; i++) {
-                  if (data[i].id == id) {
-                      data[i].description = description;
-                  }
-              }
-          } */
+    var removeTodo = function(todo) {
+      //remove from array
+      var clickedId = todo.data("id");
+      console.log(clickedId);
+      todo.splice(clickedId, 1);
+      //remove from DOM
+      todo.remove();
+      console.log(todo);
+    };
 
-        var deleteItem = function (id) {
+    var clearUI = function() {
+      $(".list").empty();
+    };
 
-        }
+    return {
+      init: function() {
+        initTodos();
 
-        var saveTodos = function (todos) {
-            localStorage.setItem("todos", JSON.stringify(todos));
+        $("#btnAdd").on("click", function() {
+          createTodo();
+          console.log(todos);
+        });
 
-        }
+        $("#btnSave").on("click", function() {
+          //addTodos(todos);
+          saveTodos(todos);
+        });
 
-        var initTodos = function () {
-            var storedTodos = JSON.parse(localStorage.getItem("todos"));
-            storedTodos.forEach(function (todo) {
-                addTodo(todo);
-            });
-        }
+        $("#btnClear").on("click", function() {
+          //addTodos(todos);
+          clearTodos(todos);
+          clearUI();
+        });
 
-        var clearTodos = function (todos) {
-            while (todos.length) {
-                todos.pop();
-            }
-        }
+        $(".list").on("click", "li", function() {
+          updateTodo($(this));
+        });
 
-        return {
-            init: function () {
-                initTodos();
-
-                $('#btnAdd').on('click', function () {
-                    createTodo();
-                    console.log(todos);
-                });
-
-
-                $('#btnSave').on('click', function () {
-                    //addTodos(todos);
-                    saveTodos(todos);
-
-                });
-
-                $('#btnClear').on('click', function () {
-                    //addTodos(todos);
-                    clearTodos(todos);
-
-                });
-                //addItem(1, "köpa glass", false);
-                /*UICtrl.displayMonth();
-                UICtrl.displayBudget({
-                    budget: 0,
-                    totalInc: 0,
-                    totalExp: 0,
-                    percentage: -1
-                });
-                setupEventListeners();*/
-            }
-        };
-
-    })();
-    TodoApp.init(); // Run application
-
+        $(".list").on("click", "span", function(e) {
+          e.stopPropagation();
+          console.log("span clicked");
+          removeTodo($(this).parent());
+        });
+      }
+    };
+  })();
+  TodoApp.init(); // Run application
 });
-
-/*
-function addTodos(todos) {
-    //add todos to page here
-    todos.forEach(function(todo){
-      addTodo(todo);
-    });
-  }
-  
-  function addTodo(todo){
-    var newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
-    newTodo.data('id', todo._id);
-    newTodo.data('completed', todo.completed);
-    if(todo.completed){
-      newTodo.addClass("done");
-    }
-    $('.list').append(newTodo);
-  }
-  */
